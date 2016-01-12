be8975062c021ce96015a02d267d52455aac6edb
Shoaib Shaikh <shoshaikh7@gmail.com>
2016-01-12 14:43:18 -0500

    Stylized forms and company page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     new_company_path
   end
+  def after_update_path_for(resource)
+    companies_path(resource)
+  end
 end
